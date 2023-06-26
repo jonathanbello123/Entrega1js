e11381a0c18499f69407bac824ebc02b49f7a407
jonathanbello123 <jonathanbellogabriel@gmail.com>
2023-06-25 21:11:28 -0300

    ultimo

--- e1.js
+++ e1.js
@@ -64,7 +64,7 @@
 // }
 // imprimirEquipos(equipos)
 
-// Ejercicio 7 
+// Ejercicio 7
 // Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo.
 // Ayuda: Recuerden que el primer índice de un array es "0".
 
@@ -83,11 +83,11 @@
 
 // Ejercicio 8
 // Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
-const arrayN = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const imprimirAyn = (a, b) => {
-    for (let index = 0; index < arrayN.length; index++) {
-        console.log(arrayN[index] * b)
-    }
-}
+// const arrayN = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const imprimirAyn = (a, b) => {
+//     for (let index = 0; index < arrayN.length; index++) {
+//         console.log(arrayN[index] * b)
+//     }
+// }
 
-imprimirAyn(arrayN, 3)
+// imprimirAyn(arrayN, 3)
